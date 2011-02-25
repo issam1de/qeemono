454d4d739bfa3c238cb0b250697b384259d05413
Mark von Zeschau <mark@tz-research.com>
2011-02-25 20:22:53 +0100

    Refac

--- qeemono-server/qeemono/message_handler/base.rb
+++ qeemono-server/qeemono/message_handler/base.rb
@@ -4,6 +4,8 @@ module Qeemono
     # This is the base class of all message handlers.
     # All message handlers have to subclass it.
     #
+    # Actual message handling must not be done here.
+    #
     class Base
 
       def handled_methods
