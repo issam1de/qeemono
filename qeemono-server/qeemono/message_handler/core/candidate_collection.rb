84718136f5c693a91e0c651a68f07ade32382b06
Mark von Zeschau <mark@tz-research.com>
2011-02-26 19:53:59 +0100

    Refac

--- qeemono-server/qeemono/message_handler/core/candidate_collection.rb
+++ qeemono-server/qeemono/message_handler/core/candidate_collection.rb
@@ -18,7 +18,7 @@ module Qeemono
         # **************************************************************
         # **************************************************************
 
-        def handle_bar3(params)
+        def handle_bar(params)
           @qsif[:logger].info "BAR!!! #{params}"
         end
 
