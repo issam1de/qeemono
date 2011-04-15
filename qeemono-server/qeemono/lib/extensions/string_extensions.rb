486bc6e561482ba9e6c170d9a9b176807a2ef510
Mark von Zeschau <mark@tz-research.com>
2011-04-15 17:25:09 +0200

    added docu added method to string extensions added string utils

--- qeemono-server/qeemono/lib/extensions/string_extensions.rb
+++ qeemono-server/qeemono/lib/extensions/string_extensions.rb
@@ -1,6 +1,16 @@
 class String
+  #
+  # Taken from Rails code
+  #
   def starts_with?(prefix)
     prefix = prefix.to_s
     self[0, prefix.length] == prefix
+  end
+
+  #
+  # Taken from Rails code
+  #
+  def ends_with?(suffix)
+    suffix.respond_to?(:to_str) && self[-suffix.length, suffix.length] == suffix
   end
 end
