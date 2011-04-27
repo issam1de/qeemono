63621d787d909247f37d82a80d54286eda564046
Mark von Zeschau <mark@tz-research.com>
2011-04-27 17:49:28 +0200

    code nicing added option added persistence message handler

--- qeemono-server/start_server.rb
+++ qeemono-server/start_server.rb
@@ -1,11 +1,16 @@
 require './qeemono/server.rb'
 
-server = Qeemono::Server.new('127.0.0.1', '8080', {:ws_debug => false, :log_file_fq_path => 'log/qeemono_server.log'})
+server = Qeemono::Server.new('127.0.0.1', '8080', {
+        :ws_debug => false,
+        :log_file_fq_path => 'log/qeemono_server.log',
+        :enable_persistence => true
+})
 
 mh_system = Qeemono::MessageHandler::Core::System.new
 mh_communication = Qeemono::MessageHandler::Core::Communication.new
 mh_cc = Qeemono::MessageHandler::Core::CandidateCollection.new
 mh_cc2 = Qeemono::MessageHandler::Core::CandidateCollection2.new
+mh_persist = Qeemono::MessageHandler::Core::Persistence.new
 
 #
 # Note: Do not change the registration below because the tests rely on it!
@@ -19,5 +24,6 @@ server.message_handler_manager.register(nil, Qeemono::MessageHandler::Core::Cand
 server.message_handler_manager.register(nil, Qeemono::MessageHandler::Core::CandidateCollection.new(:version => '1.4711'))
 server.message_handler_manager.unregister(nil, [mh_system, mh_communication])
 server.message_handler_manager.register(nil, [mh_system, mh_communication])
+server.message_handler_manager.register(nil, mh_persist) if mh_persist
 
 server.start
